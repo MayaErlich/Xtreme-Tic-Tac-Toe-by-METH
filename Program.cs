5c3690c4493544086a22e7d9f5b1bb028ca6ba1e
Maya Erlich <mayaluciaerlich@gmail.com>
2022-05-21 23:11:52 -0400

    added notes

--- Program.cs
+++ Program.cs
@@ -15,7 +15,10 @@ namespace Xtreme_Tic_Tac_Toe_by_METH
         //entire game is called board
         //9 parent tiles that each contain 9 childs tiles within
         //each child tile will have a value to represent x/o/unselected
-
+        //make gamemanager that dictates whether game has won or lost
+        //start with parent tile containing individual tic tac toe game, then create multiple, then create code that allows them to interact
+        //board will check which parent tiles won to decide who won game
+        //make abstract game manager that each tile can be implemented elsewhere using that implement within each parent tile and within the board
     {
         public static async Task Main(string[] args)
         {
